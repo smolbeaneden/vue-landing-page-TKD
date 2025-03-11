5dc431a713f09c2abd3d1c168efb8c5d755d8fe0
smolbeaneden <151964304+smolbeaneden@users.noreply.github.com>
2025-03-11 16:38:32 +0200

    gtag

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.use(VueGtag, {
+  config: { id: 'G-BV37MYV1NK' } // Replace with your Measurement ID
+})
+
 app.mount('#app')
+
