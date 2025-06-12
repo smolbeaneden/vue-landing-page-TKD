c834dada706c9e461a145eade7e4f01565ea3d93
smolbeaneden <151964304+smolbeaneden@users.noreply.github.com>
2025-06-12 19:37:45 +0300

    tkd 12.6.25

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'vue-landing-page-tkd',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'vue-landing-page-tkd',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
